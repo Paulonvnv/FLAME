f791da6ffed158de617a526357a3c756ff90991c
Brenda0281 <brenda0281@hotmail.com>
2026-08-03 12:37:42 -0500

    Create S1_casamalarica.R

---
+++ data/S1_casamalarica.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(magrittr)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+control_malaria_cases_by_person=read.csv(file="C:/Users/brend/OneDrive/Documentos/GitHub/FLAME/data/analysis/control_malaria_cases_by_person.csv")
+
